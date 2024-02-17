5d4b8f65f09a0d1d1f07af16b4dee546a82950fb
Luca G <xxlpmghdxx@gmail.com>
2024-02-17 15:57:12 +0100

    better path generation

--- src/world/Generator.cpp
+++ src/world/Generator.cpp
@@ -3,6 +3,12 @@
 #include <algorithm>
 #include <iostream>
 
+#include <delaunator.hpp>
+#include <vector>
+#include <queue>
+#include <unordered_set>
+#include <cassert>
+
 cppdungeon::world::Generator::Generator(i32 seed) : seed(seed)
 {
 }
@@ -26,6 +32,19 @@ void cppdungeon::world::Generator::generate(i32 width, i32 height, std::vector<u
             if (tiles[x + y * width] != 0)
                 continue;
         }
+    }
+    // generate paths
+    std::map<olc::vi2d, std::vector<olc::vi2d>> graph;
+    constructDelauneyTriangles(graph);
+
+    std::vector<std::pair<olc::vi2d, olc::vi2d>> spanningTree;
+    bfsSpanningTree(graph, graph.begin()->first, spanningTree);
+
+    for (auto &edge : spanningTree)
+    {
+        olc::vi2d start = edge.first;
+        olc::vi2d end = edge.second;
+        connectPoints(start, end, tiles);
     }
 }
 
@@ -62,8 +81,8 @@ void cppdungeon::world::Generator::generateRooms(std::vector<u16> &tiles)
         room.size = {w, h};
 
         olc::utils::geom2d::rect<i32> roomWithPadding;
-        roomWithPadding.pos = {x - 2, y - 2};
-        roomWithPadding.size = {w + 4, h + 4};
+        roomWithPadding.pos = {x - 4, y - 4};
+        roomWithPadding.size = {w + 8, h + 8};
 
         bool doesOverlap = false;
         for (auto &r : rooms)
@@ -88,7 +107,151 @@ void cppdungeon::world::Generator::generateRooms(std::vector<u16> &tiles)
     }
 }
 
+void cppdungeon::world::Generator::connectPoints(olc::vi2d p1, olc::vi2d p2, std::vector<u16> &tiles)
+{
+    if (p1.x == p2.x || p1.y == p2.y)
+    {
+        // If aligned horizontally
+        if (p1.x == p2.x)
+        {
+            // Connect horizontally first
+            int dy = abs(p2.y - p1.y);
+            int signY = (p2.y - p1.y > 0) ? 1 : -1;
+
+            for (int i = 0; i <= dy; ++i)
+                carve({p1.x, p1.y + i * signY}, 1, tiles);
+        }
+        // If aligned vertically
+        else
+        {
+            // Connect vertically first
+            int dx = abs(p2.x - p1.x);
+            int signX = (p2.x - p1.x > 0) ? 1 : -1;
+
+            for (int i = 0; i <= dx; ++i)
+                carve({p1.x + i * signX, p1.y}, 1, tiles);
+        }
+    }
+    // If not aligned horizontally or vertically
+    else
+    {
+        if (rand() % 2 == 0)
+        {
+            // Connect horizontally first towards p2
+            int dx = abs(p2.x - p1.x);
+            int signX = (p2.x - p1.x > 0) ? 1 : -1;
+
+            for (int i = 0; i <= dx; ++i)
+            {
+                carve({p1.x + i * signX, p1.y - 1}, 1, tiles);
+                carve({p1.x + i * signX, p1.y}, 1, tiles);
+                carve({p1.x + i * signX, p1.y + 1}, 1, tiles);
+            }
+
+            // Then connect vertically towards p2
+            int dy = abs(p2.y - p1.y);
+            int signY = (p2.y - p1.y > 0) ? 1 : -1;
+
+            for (int i = 0; i <= dy; ++i){
+                carve({p2.x - 1, p1.y + i * signY}, 1, tiles);
+                carve({p2.x, p1.y + i * signY}, 1, tiles);
+                carve({p2.x + 1, p1.y + i * signY}, 1, tiles);
+            }
+        }
+        else
+        {
+            // Connect vertically first towards p2
+            int dy = abs(p2.y - p1.y);
+            int signY = (p2.y - p1.y > 0) ? 1 : -1;
+
+            for (int i = 0; i <= dy; ++i){
+                carve({p1.x - 1, p1.y + i * signY}, 1, tiles);
+                carve({p1.x, p1.y + i * signY}, 1, tiles);
+                carve({p1.x + 1, p1.y + i * signY}, 1, tiles);
+            }
+            // Then connect horizontally towards p2
+            int dx = abs(p2.x - p1.x);
+            int signX = (p2.x - p1.x > 0) ? 1 : -1;
+
+            for (int i = 0; i <= dx; ++i){
+                carve({p1.x + i * signX, p2.y - 1}, 1, tiles);
+                carve({p1.x + i * signX, p2.y}, 1, tiles);
+                carve({p1.x + i * signX, p2.y + 1}, 1, tiles);
+            }
+        }
+    }
+}
+
 void cppdungeon::world::Generator::carve(olc::vi2d pos, u16 tile, std::vector<u16> &tiles)
 {
     tiles[pos.y * width + pos.x] = tile;
+}
+
+void cppdungeon::world::Generator::carveRect(olc::utils::geom2d::rect<i32> rect, u16 tile, std::vector<u16> &tiles)
+{
+    for (i32 x = rect.pos.x; x < rect.pos.x + rect.size.x; x++)
+    {
+        for (i32 y = rect.pos.y; y < rect.pos.y + rect.size.y; y++)
+        {
+            carve(olc::vi2d(x, y), tile, tiles);
+        }
+    }
+}
+
+void cppdungeon::world::Generator::constructDelauneyTriangles(std::map<olc::vi2d, std::vector<olc::vi2d>> &graph)
+{
+    graph.clear();
+    std::vector<double> coords;
+    for (auto &r : rooms)
+    {
+        coords.push_back(r.middle().x);
+        coords.push_back(r.middle().y);
+    }
+    delaunator::Delaunator d(coords);
+    for (i32 i = 0; i < d.triangles.size(); i += 3)
+    {
+        i32 i0 = d.triangles[i];
+        i32 i1 = d.triangles[i + 1];
+        i32 i2 = d.triangles[i + 2];
+        olc::vi2d p0 = {(i32)coords[i0 * 2], (i32)coords[i0 * 2 + 1]};
+        olc::vi2d p1 = {(i32)coords[i1 * 2], (i32)coords[i1 * 2 + 1]};
+        olc::vi2d p2 = {(i32)coords[i2 * 2], (i32)coords[i2 * 2 + 1]};
+        graph[p0].push_back(p1);
+        graph[p0].push_back(p2);
+        graph[p1].push_back(p0);
+        graph[p1].push_back(p2);
+        graph[p2].push_back(p0);
+        graph[p2].push_back(p1);
+    }
+}
+
+void cppdungeon::world::Generator::bfsSpanningTree(std::map<olc::vi2d, std::vector<olc::vi2d>> &graph, const olc::vi2d &start, std::vector<std::pair<olc::vi2d, olc::vi2d>> &spanningTree)
+{
+    spanningTree.clear();
+    std::queue<olc::vi2d> q;
+    std::unordered_set<olc::vi2d, VI2dHash> visited;
+
+    q.push(start);
+    visited.insert(start);
+
+    while (!q.empty())
+    {
+        olc::vi2d current = q.front();
+        q.pop();
+        for (const olc::vi2d &neighbor : graph.at(current))
+        {
+            if (visited.find(neighbor) == visited.end())
+            {
+                // add node that we havent visited yet
+                spanningTree.push_back({current, neighbor});
+                q.push(neighbor);
+                visited.insert(neighbor);
+            }
+            else if (rand() % 100 < loopProbability)
+            {
+                // add node that we have visited before
+                spanningTree.push_back({current, neighbor});
+            }
+        }
+    }
 }
--- src/world/Generator.hpp
+++ src/world/Generator.hpp
@@ -5,6 +5,7 @@
 #define OLC_IGNORE_VEC2D
 #include "olcUTIL_Geometry2D.h"
 #include <vector>
+#include <map>
 
 namespace cppdungeon
 {
@@ -32,15 +33,47 @@ private:
     // room generation
     std::vector<olc::utils::geom2d::rect<i32>> rooms;
     i32 numRoomTries = 1000;
-    f32 meanRoomSize = 15;
+    f32 meanRoomSize = 17;
     f32 roomSizeVariance = 10;
 
+    // path generation
+    class VI2dHash
+    {
+    private:
+        const int HASH_SIZE = 100;
+        const float L = 0.2f;
+
+    public:
+        std::size_t operator()(olc::vi2d const &v) const
+        {
+            int ix = (unsigned int)((v.x + 2.f) / L);
+            int iy = (unsigned int)((v.y + 2.f) / L);
+            return (unsigned int)((ix * 73856093) ^ (iy * 19349663)) % HASH_SIZE;
+        }
+    };
+    i32 loopProbability = 10;
+
     void generateRooms(std::vector<u16> &tiles);
-    void carve(olc::vi2d pos, u16 tile, std::vector<u16> &tiles);
+    void carve(olc::vi2d pos,
+               u16 tile,
+               std::vector<u16> &tiles);
+    void connectPoints(olc::vi2d a,
+                       olc::vi2d b,
+                       std::vector<u16> &tiles);
+    void carveRect(olc::utils::geom2d::rect<i32> rect,
+                   u16 tile,
+                   std::vector<u16> &tiles);
+    void constructDelauneyTriangles(std::map<olc::vi2d,
+                                             std::vector<olc::vi2d>> &graph);
+    void bfsSpanningTree(std::map<olc::vi2d, std::vector<olc::vi2d>> &graph,
+                         const olc::vi2d &start,
+                         std::vector<std::pair<olc::vi2d, olc::vi2d>> &spanningTree);
 
 public:
     Generator(i32 seed);
-    void generate(i32 width, i32 height, std::vector<u16> &tiles);
+    void generate(i32 width,
+                  i32 height,
+                  std::vector<u16> &tiles);
 };
 
 #endif
